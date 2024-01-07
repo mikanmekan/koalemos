36bc155894562554780733911dc0f0f19c2f1bdc
mikanmekan <mikanmekanhub@gmail.com>
2024-01-07 20:02:26 +0000

    add helper funcs

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -1,5 +1,11 @@
 package metrics
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // MetricPoint represents a single Koalemos data model metric datum.
 type MetricPoint struct {
 	Name      string
@@ -8,10 +14,46 @@ type MetricPoint struct {
 	Timestamp int64
 }
 
+func (m *MetricPoint) String() string {
+	sb := strings.Builder{}
+
+	metricPoint := "Name: " + m.Name + "\n" +
+		"Value: " + "%f" + "\n" +
+		"Name: " + m.Name + "\n" +
+		"Timestamp: " + "%d" + "\n" +
+		"Labels:"
+
+	floatStr := strconv.FormatFloat(m.Value, 'f', 4, 64)
+	metricPoint = fmt.Sprintf(metricPoint, floatStr, m.Timestamp)
+
+	sb.WriteString(metricPoint)
+	for k, v := range m.LabelSet {
+		sb.WriteString(fmt.Sprintf(" {%s: %s}", k, v))
+	}
+
+	return sb.String()
+}
+
 // MetricFamily represents a group of metrics.
 type MetricFamily struct {
 	Name    string
 	Metrics []MetricPoint
 	Type    string
 	Help    string
+}
+
+func (m *MetricFamily) String() string {
+	sb := strings.Builder{}
+
+	metricFamily := "Name: " + m.Name + "\n" +
+		"Type: " + m.Type + "\n" +
+		"Help: " + m.Help + "\n" +
+		"Metrics:"
+
+	sb.WriteString(metricFamily)
+	for _, v := range m.Metrics {
+		sb.WriteString(v.String())
+	}
+
+	return sb.String()
 }
