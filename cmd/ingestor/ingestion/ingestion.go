6d3ba454ac9b0b1eb21f36b4406abc15c1737e02
mikanmekan <mikanmekanhub@gmail.com>
2024-01-07 19:04:56 +0000

    tidy files

--- cmd/ingestor/ingestion/ingestion.go
+++ cmd/ingestor/ingestion/ingestion.go
@@ -44,9 +44,6 @@ func (i *Ingestor) HandleMetrics(w http.ResponseWriter, r *http.Request) {
 	i.logger.Info(fmt.Sprintf("%v", mfs))
 
 	w.WriteHeader(http.StatusOK)
-
-	// mfStr := fmt.Sprintf("%v", mfs)
-	// w.Write([]byte(mfStr))
 }
 
 func (i *Ingestor) Register(r *mux.Router) {
